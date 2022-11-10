643b284520a73986c949f265ec4027ba4f14c648
herley <herley@gmail.com>
2022-11-10 10:10:01 +0700

    second commit.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World Dunia."}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
